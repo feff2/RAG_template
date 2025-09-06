a329b9f1a89b439fade75359a2ea9040b4905913
Nikita Koshelev <nikita.koshelev@shkulev.ru>
2025-09-06 19:06:08 +0700

    New truncated logic

--- src/services/api_gateway/schemes.py
+++ src/services/api_gateway/schemes.py
@@ -4,28 +4,27 @@ from pydantic import BaseModel
 
 
 class QueryIn(BaseModel):
-    request_id: str
+    user_id: str
     query: str
 
 
 class QueryOut(BaseModel):
-    request_id: str
+    user_id: str
     response: str
 
 
 class PipelineIn(BaseModel):
-    request_id: str
+    user_id: str
     query: str
     history_session: str
 
 
 class PipelineOut(BaseModel):
-    request_id: str
+    user_id: str
     generated: str
 
 
 class FeedbackIn(BaseModel):
-    request_id: str
     user_id: str
     user_message: str
     model_response: str
